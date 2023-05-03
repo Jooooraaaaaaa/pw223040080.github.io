2a819261832f07b5385a1b72d1f2d7a4585ab27b
Jooooraaaaaaa <104358370+Jooooraaaaaaa@users.noreply.github.com>
2023-05-03 13:22:07 +0700

    Add files via upload

---
+++ tubes/halamanutama.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>TechnoNews</title>
+    <link rel="stylesheet" href="styleH.css">
+</head>
+<body>
+    <header>
+        <div>
+            <img src="logo.png" alt="">
+                </div>
+            <ul class="nav">
+                <li><a href="halamanutama.php" class="active">Home</a></li> 
+                <li><a href="/">Baru</a></li>
+                <li><a href="/">Viral</a></li>
+                <li><a href="/">Kategori</a></li>
+            </ul>
+                <div class="login">
+                    <a href="register.php">Daftar</a>
+                    <a href="logout.php">Logout</a>
+                </div>
+        </div>
+    </header>
+       
+    
+</body>
+</html>
